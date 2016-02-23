a5bf960d14e1b58b19b7f567ed981be2d068346a
2betop <liaoxuezhi@icloud.com>
2016-02-23 09:24:23 +0000

    rm all files

--- _Uint8Array.js
+++
@@ -1,4 +0,0 @@
-/** Built-in value references. */
-var Uint8Array = window.Uint8Array;
-
-module.exports = Uint8Array;
--- internal/_Symbol.js
+++
@@ -1,4 +0,0 @@
-/** Built-in value references. */
-var _Symbol = window.Symbol;
-
-module.exports = _Symbol;
